258de8449fa6a11f24b86438e266a9e5aed4272d
Homenique Vieira <homenique.t@gmail.com>
2022-04-21 21:20:07 -0300

    [Create] Creating the basic class and doing the graysscale operation

---
+++ src/1. Basic/basic.py
@@ -0,0 +1,13 @@
+import cv2 as cv 
+
+img = cv.imread('../../resources/images/calibration.jpg')
+
+## Show image in a windows
+cv.imshow('Image window', img)
+
+# Converting to grayscale 
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+cv.imshow('Gray', gray)
+
+cv.waitKey(0)
