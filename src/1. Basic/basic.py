3f554918bd0c1a9fd2d38316d7b3ba18d4b719ed
Homenique Vieira <homenique.t@gmail.com>
2022-04-21 21:26:14 -0300

    [add]New image

--- src/1. Basic/basic.py
+++ src/1. Basic/basic.py
@@ -3,7 +3,7 @@ import cv2 as cv
 img = cv.imread('../../resources/images/calibration.jpg')
 
 ## Show image in a windows
-cv.imshow('Image window', img)
+cv.imshow('calibration.jpg', img)
 
 # Converting to grayscale 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
